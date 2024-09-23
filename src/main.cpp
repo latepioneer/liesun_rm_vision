a83293701cb5b83402799acbb27813dd390d8d6a
wtl <3398767646@qq.com>
2024-09-23 10:58:06 +0800

    源文件

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include "my_thread.h"
+#include "uart.h"
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    Task task;
+    ThreadPool pool(2); // 创建一个具有2线程的线程池
+
+    pool.enqueue([&task]
+                 { task.camera_task(); });
+
+    pool.enqueue([&task]
+                 { task.get_armor_task(); });
+
+    
+    return 0;
+}
